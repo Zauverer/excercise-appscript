f4ab9e12306eac7632751c40835ea6d50059ada7
Cristian <56987681+Zauverer@users.noreply.github.com>
2023-05-23 16:01:36 -0400

    first commit

---
+++ algorithms.py
@@ -0,0 +1,14 @@
+#SWAPPING NUMBERS
+
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+print("Value num 1 is: ", num1)
+print("Value num 2 is: ", num2)
+
+#swaping numbers
+
+num1, num2 = num2, num1
+
+print("Value num 1 after swapping is: ", num1)
+print("Value num 2 after swapping is: ", num2)
